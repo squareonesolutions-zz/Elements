f2a55162805f686c53ee2c6c8a5e2c241a9d4645
Adam Schuster <adam@bewarethesquare.com>
2013-07-18 16:28:38 -1000

    Initial Commit
    Website files, as adopted from their previous web designer and and
    improved on

---
+++ scripts/smooth.pack.js
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<HTML><HEAD>
+<TITLE>300 Multiple Choices</TITLE>
+</HEAD><BODY>
+<H1>Multiple Choices</H1>
+The document name you requested (<code>/scripts/smooth.pack.js</code>) could not be found on this server.
+However, we found documents with names similar to the one you requested.<p>Available documents:
+<ul>
+<li><a href="smooth.js">/scripts/smooth.js</a> (common basename)
+</ul>
+Please consider informing the owner of the <a href="../salon_services.html">referring page</a> about the broken link.
+</BODY></HTML>
